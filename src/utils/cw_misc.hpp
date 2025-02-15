7133bac556023bc9d3e9722e37411a9a3dd35955
Ashley Zhang <ayz27@cornell.edu>
2025-02-15 13:52:57 -0500

    unsuccessfully move logging to separate file

---
+++ src/utils/cw_misc.hpp
@@ -0,0 +1,5 @@
+// ==================================================================
+// Author: Ashley Zhang (ayz27@cornell.edu)
+// Date:   2/15/2025
+// Description: cw misc functions and objects not deserving of separate file
+// ==================================================================
