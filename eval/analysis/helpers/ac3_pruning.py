526f996208303bd3c875e08a444efc31c55f4204
Ashley Zhang <ayz27@cornell.edu>
2025-04-26 17:14:34 -0400

    add dependent var getter

--- eval/analysis/helpers/ac3_pruning.py
+++ eval/analysis/helpers/ac3_pruning.py
@@ -38,15 +38,31 @@ def analyze_ac3_pruning_durations(data, img_filepath):
     # plt.legend()
     plt.show()
 
-# return track_ac3 flag
-def track_ac3(data) -> bool:
+# getter for track_ac3 flag
+def get_track_ac3(data) -> bool:
     assert('type' in data and data.get('type') == "Total")
     assert('result' in data)
     assert('track_ac3' in data.get('result'))
     return data.get('result').get('track_ac3')
 
+# getter for constr_dependent_vars entry
+def get_dependent_vars(data, constr_id) -> list[int]:
+    assert('type' in data and data.get('type') == "Total")
+    assert('children' in data)
+    children = data.get('children', [])
+    assert(len(children) > 0)
+    assert('type' in children[0] and children[0].get('type') == "Initialize")
+    assert('result' in children[0])
+    assert('constr_dependent_vars' in children[0].get('result'))
+
+    constr_dependent_vars = children[0].get('result').get('constr_dependent_vars')
+    assert(str(constr_id) in constr_dependent_vars)
+    return constr_dependent_vars.get(str(constr_id))
+
+# run all child functions, return true iff ac3 pruning was tracked
 def analyze_ac3_pruning(data) -> bool:
-    if not track_ac3(data):
+    if not get_track_ac3(data):
         print("Warning: AC-3 not tracked in provided file, skipping")
         return False
+
     return True
