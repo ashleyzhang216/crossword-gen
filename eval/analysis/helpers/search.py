7077bbad65465f49a06b09cc795b9d198243e4c8
Ashley Zhang <ayz27@cornell.edu>
2025-05-12 19:11:50 -0400

    add backjump edges on search tree diagram

--- eval/analysis/helpers/search.py
+++ eval/analysis/helpers/search.py
@@ -45,12 +45,24 @@ class SearchNode:
 
     def set_jump_height(self, jump_height):
         assert(self.jump_height is None)
-        self.jump_height = 0 if jump_height is None else jump_height
+        if self.success:
+            assert(jump_height is None)
+        self.jump_height = jump_height
 
     def set_reason_solved(self):
         assert(not self.reason is None)
         assert(not self.reason is SearchReason.SOLVED)
         self.reason = SearchReason.SOLVED
+
+    def get_node_jumped_to(self):
+        assert(not self.jump_height is None)
+        assert(self.jump_height > 0)
+        current = self
+        for i in range(self.jump_height):
+            current = current.parent
+            if not i == self.jump_height - 1:
+                assert(current.jump_height is None)
+        return current
 
     def get_color(self) -> str:
         assert(not self.reason is None)
@@ -82,7 +94,7 @@ def gather_search_tree(data) -> SearchNode:
     tree = SearchNode(0, search_root['result']['success'], SearchReason(search_root['result']['reason']))
     next_id = 1
 
-    def traverse(search_node, data_node):
+    def traverse(search_node:SearchNode, data_node):
         nonlocal next_id
         if data_node['type'] == "Try Assign":
             node_child = search_node.add_child(next_id, data_node['result']['success'], SearchReason(data_node['result']['reason']))
@@ -90,6 +102,12 @@ def gather_search_tree(data) -> SearchNode:
 
             for child_data_node in data_node.get('children', []):
                 traverse(node_child, child_data_node)
+
+            # compensate for leaf nodes in a dead end currently not being annotated with jump_height due to no search step underneath
+            if len(node_child.children) == 0 and not node_child.success:
+                assert(node_child.reason == SearchReason.AC3 or node_child.reason == SearchReason.DUPLICATE)
+                assert(node_child.jump_height is None)
+                node_child.set_jump_height(1)
         else:
             if data_node['type'] == "Search Step":
                 if data_node['result']['success'] and data_node['result']['reason'] == "solved":
@@ -100,8 +118,33 @@ def gather_search_tree(data) -> SearchNode:
             for child_data_node in data_node.get('children', []):
                 traverse(search_node, child_data_node)
 
+    def verify_jump_heights(root:SearchNode):
+        def verify_node(node:SearchNode):
+            # node that failed and isn't root
+            if not node.parent is None and not node.success:
+                assert(not node.jump_height is None)
+                current = node
+                for i in range(node.jump_height):
+                    current = current.parent
+                    assert(not current is None)
+                    if not i == node.jump_height - 1:
+                        assert(current.jump_height is None)
+                verify_node(current)
+            elif node.success:
+                assert(node.jump_height is None)
+
+        def call_from_leaves(node:SearchNode):
+            if len(node.children) == 0:
+                verify_node(node)
+            else:
+                for child in node.children:
+                    call_from_leaves(child)
+
+        call_from_leaves(root)
+
     traverse(tree, data)
     tree.set_failed_subtree_size()
+    verify_jump_heights(tree)
 
     return tree
 
@@ -117,10 +160,19 @@ def plot_search_tree(output_dir, search_tree:SearchNode):
         assert(not node.id in all_ids)
         all_ids.add(node.id)
 
-        dot.node(str(node.id), node.reason.value, color=node.get_color())
-        for child in node.children:
+        jump_h = "None" if node.jump_height is None else str(node.jump_height)
+        dot.node(str(node.id), jump_h, color=node.get_color())
+        for i, child in enumerate(node.children):
+            # forward edge to child
             dot.edge(str(node.id), str(child.id))
-            # TODO: draw edge up to where it backtracked/backjumped
+
+            # backjump edge from child
+            if not child.jump_height is None:
+                if child.jump_height > 1:
+                    assert(i == len(node.children) - 1)
+
+                dot.edge(str(child.id), str(child.get_node_jumped_to().id))
+
             add_node_edges(child)
 
     add_node_edges(search_tree)
