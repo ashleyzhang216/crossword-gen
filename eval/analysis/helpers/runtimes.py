85021c93a29abd0d42ab0e34837551df71c894b7
Ashley Zhang <ayz27@cornell.edu>
2025-07-12 19:24:37 -0400

    update analysis scripts with refactored timestep name

--- eval/analysis/helpers/runtimes.py
+++ eval/analysis/helpers/runtimes.py
@@ -24,8 +24,8 @@ def gather_init_search_runtime_data(data):
         "search": search_node.get('duration_us') * 1e-6
     }
 
-# returns map of timestep name -> list of durations
-def gather_ts_runtime_data(data):
+# returns map of span name -> list of durations
+def gather_span_runtime_data(data):
     result = {}
 
     def traverse(node, result:dict) -> int:
@@ -66,29 +66,29 @@ def plot_init_search_runtimes(output_dir, init_search_runtime_data):
     plt.savefig(output_dir + 'init_search_runtimes.png', bbox_inches='tight')
     plt.close()
 
-# plot total runtime distribution by timestep type
-def plot_total_runtime_by_timestep_type(output_dir, ts_runtime_data):
-    if ts_runtime_data is None:
+# plot total runtime distribution by span type
+def plot_total_runtime_by_span_type(output_dir, span_runtime_data):
+    if span_runtime_data is None:
         return
 
-    ts_types = np.asarray(sorted(ts_runtime_data, key=lambda k: sum(ts_runtime_data[k]), reverse=True))
-    durations_us = np.asarray([sum(ts_runtime_data[t]) for t in ts_types])
-    durations_s = np.asarray([durations_us[i] * 1e-6 for i in range(len(ts_types))])
-    percents = np.asarray([100 * sum(ts_runtime_data[t]) / sum(durations_us) for t in ts_types])
+    span_types = np.asarray(sorted(span_runtime_data, key=lambda k: sum(span_runtime_data[k]), reverse=True))
+    durations_us = np.asarray([sum(span_runtime_data[t]) for t in span_types])
+    durations_s = np.asarray([durations_us[i] * 1e-6 for i in range(len(span_types))])
+    percents = np.asarray([100 * sum(span_runtime_data[t]) / sum(durations_us) for t in span_types])
 
     plt.figure(figsize=(10, 6))
 
-    bars = plt.bar(ts_types, durations_s, edgecolor='black')
+    bars = plt.bar(span_types, durations_s, edgecolor='black')
     plt.bar_label(bars,
               labels=[f'{p:.2f}%\n({s:.2f} sec)\n' for p, s in zip(percents, durations_s)],
               padding=3,
               fontsize=9)
 
     plt.ylim(0, 1.2 * max(durations_s))
-    plt.xticks(ts_types)
+    plt.xticks(span_types)
 
     plt.annotate(
-        "Total time for each timestep type\nexcluding total duration of children",
+        "Total time for each span type\nexcluding total duration of children",
         xy=(0.98, 0.98),
         xycoords='axes fraction',
         ha='right',
@@ -96,11 +96,11 @@ def plot_total_runtime_by_timestep_type(output_dir, ts_runtime_data):
         bbox=dict(boxstyle='round', pad=0.4, facecolor='white', alpha=0.8, edgecolor='0.8')
     )
 
-    plt.title('Total Duration vs Timestep Type')
-    plt.xlabel('Timestep Type')
+    plt.title('Total Duration vs Span Type')
+    plt.xlabel('Span Type')
     plt.ylabel('Total Duration (s)')
 
-    plt.savefig(output_dir + 'total_runtime_by_timestep_type.png', bbox_inches='tight')
+    plt.savefig(output_dir + 'total_runtime_by_span_type.png', bbox_inches='tight')
     plt.close()
 
 #################### parent function ####################
@@ -111,10 +111,10 @@ def analyze_runtimes(data, output_dir) -> bool:
         print("Warning: AC-3 not tracked in provided file, skipping some analysis of runtimes")
 
     init_search_runtime_data = gather_init_search_runtime_data(data)
-    ts_runtime_data = gather_ts_runtime_data(data) if get_track_ac3(data) else None
+    span_runtime_data = gather_span_runtime_data(data) if get_track_ac3(data) else None
 
     plot_init_search_runtimes(output_dir, init_search_runtime_data)
-    plot_total_runtime_by_timestep_type(output_dir, ts_runtime_data)
+    plot_total_runtime_by_span_type(output_dir, span_runtime_data)
 
     with open(output_dir + 'runtimes_metrics.md', 'w') as file:
         file.write("## Runtimes Metrics\n\n")
@@ -125,13 +125,13 @@ def analyze_runtimes(data, output_dir) -> bool:
         file.write(f"| {init_search_runtime_data['init']+init_search_runtime_data['search']:.3f} | {init_search_runtime_data['init']:.2f} | {init_search_runtime_data['search']:.2f} |\n")
 
         if get_track_ac3(data):
-            file.write("### Timestep type durations (excluding children)\n")
+            file.write("### Span type durations (excluding children)\n")
 
-            ts_types = sorted(ts_runtime_data, key=lambda k: sum(ts_runtime_data[k]), reverse=True)
-            file.write("| Timestep | Count | Total (s) | Min (μs) | Max (μs) | Average (μs) | Median (μs) |\n")
-            file.write("|----------|-------|-----------|----------|----------|--------------|-------------|\n")
-            for t in ts_types:
-                d = ts_runtime_data[t]
+            span_types = sorted(span_runtime_data, key=lambda k: sum(span_runtime_data[k]), reverse=True)
+            file.write("| Span | Count | Total (s) | Min (μs) | Max (μs) | Average (μs) | Median (μs) |\n")
+            file.write("|------|-------|-----------|----------|----------|--------------|-------------|\n")
+            for t in span_types:
+                d = span_runtime_data[t]
                 assert(len(d) > 0)
                 file.write(f"| {t} | {len(d)} | {sum(d)*1e-6:.2f} | {min(d)} | {max(d)} | {sum(d)/len(d):.2f} | {np.median(d):.1f} |\n")
 
