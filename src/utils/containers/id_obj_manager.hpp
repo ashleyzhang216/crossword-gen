1fef11fe92b65f61c7a193acf3a3285ba14d3f6b
Ashley Zhang <ayz27@cornell.edu>
2025-08-05 19:41:43 -0400

    remove optionalness of id_obj_manager's vector

--- src/utils/containers/id_obj_manager.hpp
+++ src/utils/containers/id_obj_manager.hpp
@@ -47,25 +47,20 @@ namespace cw {
 
             // add element
             void push_back(unique_ptr<T>&& ptr) {
-                if(!vec.has_value()) {
-                    vec = vector<unique_ptr<T> >();
-                }
-
-                cw_assert(vec.value().size() == ptr->id);
-                vec.value().push_back(std::move(ptr));
+                cw_assert(vec.size() == ptr->id);
+                vec.push_back(std::move(ptr));
             }
 
             // access size
             size_t size() const {
-                if(!vec.has_value()) return 0ul;
-                return vec.value().size();
+                return vec.size();
             }
 
             // access vector element
             unique_ptr<T>& operator[](size_t n) {
-                cw_assert(vec.has_value());
-                cw_assert(vec.value()[n]->id == n);
-                return vec.value()[n];
+                cw_assert(n < vec.size());
+                cw_assert(vec[n]->id == n);
+                return vec[n];
             }
             const unique_ptr<T>& operator[](size_t n) const {
                 return const_cast<const unique_ptr<T>&>(const_cast<id_obj_manager*>(this)->operator[](n));
@@ -85,33 +80,23 @@ namespace cw {
             using const_iterator = typename vector<unique_ptr<T> >::const_iterator;
 
             // iterators
-            iterator begin() {
-                return vec.has_value() ? vec->begin() : empty_vec.begin();
-            }
-            const_iterator begin() const {
-                return vec.has_value() ? vec->begin() : empty_vec.begin();
-            }
-            iterator end() {
-                return vec.has_value() ? vec->end() : empty_vec.end();
-            }
-            const_iterator end() const {
-                return vec.has_value() ? vec->end() : empty_vec.end();
-            }
+            iterator       begin()       { return vec.begin(); }
+            iterator       end()         { return vec.end();   }
+            const_iterator begin() const { return vec.begin(); }
+            const_iterator end()   const { return vec.end();   }
 
             // deep copy
             id_obj_manager clone() const {
                 id_obj_manager copy;
 
-                if(vec.has_value()) {
-                    vector<unique_ptr<T> > vec_copy;
-                    vec_copy.reserve(vec->size());
+                vector<unique_ptr<T> > vec_copy;
+                vec_copy.reserve(size());
 
-                    for(const auto& ptr : vec.value()) {
-                        vec_copy.emplace_back(ptr->clone());
-                    }
-
-                    copy.init(std::move(vec_copy));
+                for(const auto& ptr : vec) {
+                    vec_copy.emplace_back(ptr->clone());
                 }
+
+                copy.init(std::move(vec_copy));
                 return copy;
             }
 
@@ -121,39 +106,27 @@ namespace cw {
 
             // movable
             id_obj_manager(id_obj_manager&& other) : id_obj_manager() {
-                if(other.vec.has_value()) {
-                    init(std::move(other.vec.value()));
-                }
+                init(std::move(other.vec));
             }
             id_obj_manager& operator=(id_obj_manager&& other) {
-                cw_assert(!vec.has_value()); // cannot hold existing objects
-                if(other.vec.has_value()) {
-                    init(std::move(other.vec.value()));
-                }
+                cw_assert(size() == 0); // cannot hold existing objects
+                init(std::move(other.vec));
                 return *this;
             }
 
         protected:
             // underlying data
-            optional<vector<unique_ptr<T> > > vec;
-
-            // fallback for iterators when vec is empty
-            static vector<unique_ptr<T> > empty_vec;
+            vector<unique_ptr<T> > vec;
 
             // initialize with data
             void init(vector<unique_ptr<T> >&& data) {
-                cw_assert(!vec.has_value());
+                cw_assert(size() == 0);
                 vec = std::move(data);
                 for(size_t i = 0; i < size(); ++i) {
-                    cw_assert(vec.value()[i]->id == i);
+                    cw_assert(vec[i]->id == i);
                 }
             }
-    };
-
-    // define empty_vec
-    template <class T>
-    requires std::conjunction_v<has_id<T, size_t>, has_clone<T> >
-    vector<unique_ptr<T> > id_obj_manager<T>::empty_vec = {};
+    }; // id_obj_manager
 } // namespace cw
 
 #endif // CW_ID_OBJ_MANAGER_HPP
